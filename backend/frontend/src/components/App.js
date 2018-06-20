c368228d61af1f87b56af8bd46740dff127d42b4
haidarzxc <haidar.a.alanbari@gmail.com>
2018-06-20 13:25:31 -0400

    bootstrap library

--- backend/frontend/src/components/App.js
+++ backend/frontend/src/components/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../css/App.css';
+import { Grid,Row,Col,ProgressBar } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          zxc
-        </p>
-      </div>
+    <Grid>
+      <Row>
+        {/* sideBar */}
+        <Col xd={2} md={2}>
+          
+        </Col>
+        
+        {/* body */}
+        <Col xd={10} md={10}>
+        
+        </Col>
+      </Row>
+    </Grid>
     );
   }
 }
